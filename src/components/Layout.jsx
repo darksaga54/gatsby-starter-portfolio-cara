f1e4e63f5fcd373603ebe52990cd0aedeaca84fb
Cultra <m.tony.001@outlook.com>
2019-05-28 21:01:31 -0700

    GrapeVine

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,11 +3,13 @@ import GlobalStyle from '../styles/global'
 import SEO from './SEO'
 import 'typeface-cantata-one'
 import 'typeface-open-sans'
+import Header from '../components/Header.jsx'
 
 const Layout = () => (
   <>
     <SEO />
     <GlobalStyle />
+    <Header/>
   </>
 )
 
